121ddec69507f35861ac7dc8f38aa42ed41cc414
nada-aldubaie2 <126459665+nada-aldubaie2@users.noreply.github.com>
2025-04-07 14:31:03 -0300

    gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://vcf-cleaner.vercel.app/"
+    "https://vcf-cleaner.vercel.app"
 ]
 
 CORS_EXPOSE_HEADERS = ['Content-Disposition']
@@ -87,14 +87,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
